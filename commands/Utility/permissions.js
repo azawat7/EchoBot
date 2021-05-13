4c9ea78b615db1c7a22af0e8548bc727a91a3148
Anthony Jezierska-Gomes <az4wat@gmail.com>
2021-05-13 20:05:29 +0200

    Commit #26 - permissions.js

---
+++ commands/Utility/permissions.js
@@ -0,0 +1,39 @@
+const { Permissions } = require("discord.js");
+const permissions = Object.keys(Permissions.FLAGS);
+
+module.exports.help = {
+  name: "permissions",
+  aliases: ['permission', 'perms'],
+  category: "utility",
+  description: "List your permission in a channel",
+  expectedArgs: "\`<@user>\`",
+  minArgs: 0,
+  maxArgs: 1,
+  ownerOnly: false,
+  userPerms: [],
+  clientPerms: [],
+  nsfw: false,
+  cooldown: 3
+}
+
+module.exports.run = async (client, message, args) => {
+        const member = message.mentions.members.first() || message.member;
+		let text = `\`\`\`${member.user.username}\'s permissions in ${message.channel.name} :\n\n`;
+		const mPermissions = message.channel.permissionsFor(member);
+		const total = {
+			denied: 0,
+			allowed: 0
+		};
+		permissions.forEach((perm) => {
+			if(!mPermissions.has(perm)){
+				text += `${perm} ❌\n`;
+				total.denied++;
+			} else {
+				text += `${perm} ✅\n`;
+				total.allowed++;
+			}
+		});
+		text += `\n${total.allowed} ✅ | ${total.denied} ❌`+"\n```";
+		message.channel.send(text);
+	};
+    
