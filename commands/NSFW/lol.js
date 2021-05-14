30ede553ab03bb4a2f2a52f5511a85ab16079b40
Anthony Jezierska-Gomes <az4wat@gmail.com>
2021-05-14 12:08:56 +0200

    Commit #29 - lol.js

---
+++ commands/NSFW/lol.js
@@ -0,0 +1,46 @@
+const fetch = require('node-fetch');
+
+module.exports.help = {
+	name: "leagueoflegends",
+	aliases: [ 'lol', 'league'],
+	category: "NSFW",
+	description: "ye ye",
+	expectedArgs: null,
+	minArgs: 0,
+	maxArgs: 0,
+	ownerOnly: false,
+	userPerms: [],
+	clientPerms: [],
+	nsfw: true,
+	cooldown: 1
+}
+
+module.exports.run = async (client, message, args) => {
+        try {
+          var subreddits = [
+            'Rule34LoL',
+          ]
+  
+          var reddit = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
+        
+          const data = await fetch(`https://meme-api.herokuapp.com/gimme/${reddit}`).then(res => res.json())
+  
+          if (!data) return message.channel.send(`Sorry, seems like i can't connect to API.`);
+        
+          const { title, postLink, url, subreddit } = data
+  
+          message.channel.send({
+            embed: {
+              color: "BLURPLE",
+              title: `${title}`,
+              url: `${postLink}`,
+              image: {
+                url: url
+              },
+              footer: { text: `/reddit/${subreddit}` }
+            }
+          });
+        } catch(error) {
+          client.emit("apiError", error, message);
+        }
+    }
