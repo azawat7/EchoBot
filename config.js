ad380d2a56f3e296d8b5815f230feb401691348c
Anthony Jezierska-Gomes <az4wat@gmail.com>
2021-05-18 19:15:17 +0200

    Commit #42 - fixes

---
+++ config.js
@@ -0,0 +1,12 @@
+exports.token = "ODM4MDYxOTM1MDM5NjEwOTIx.YI1n_w.kas1xOv4env9Fkcs-YR3ifhqYFY";
+exports.prefix = "$";
+exports.owners = ['406108846575058944'];
+exports.MONGOPATH = "mongodb+srv://root:THr42F1tVuHqsOVe@echo-bot.b6y3f.mongodb.net/EchoDB?retryWrites=true&w=majority"
+exports.colors = {
+    "echo": "#FF0000"
+}
+exports.DEFAULTSETTINGS = {
+    prefix: "$",
+    language: "english"
+};
+            
